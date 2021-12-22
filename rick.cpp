b3148dd25289f97a22408640c27eeb5f8520b62d
normie <47870580+snthcy@users.noreply.github.com>
2021-12-22 16:12:52 -0500

    refined some code
    simplified while loop
    ShellExecute was messed up for some reason, did some research and fixed it.

--- rick.cpp
+++ rick.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 int main()
 {
-	bool truth = true;
-	FreeConsole();
-	while (truth == true) {
-		if (GetAsyncKeyState('E') & 0x8000) {
-			ShellExecute(0, 0, "https://www.youtube.com/watch?v=dQw4w9WgXcQ", 0, 0, SW_SHOW);
-		}
-	}
+    FreeConsole();
+    while (true) {
+        if (GetAsyncKeyState('E') & 0x8000) {
+            ShellExecute(0, 0, L"https://www.youtube.com/watch?v=dQw4w9WgXcQ", 0, 0, SW_SHOW);
+        }
+    }
     return 0;
-}
+};
